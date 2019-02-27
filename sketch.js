97000b8de8572facc9139fb843ead73dad4df618
Dani Castilho <danicastilhodev@gmail.com>
2019-02-27 05:36:29 -0300

    Random radius and colors

--- sketch.js
+++ sketch.js
@@ -1,40 +1,52 @@
 const canvasSketch = require('canvas-sketch');
 const {lerp} = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const palletes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
 const sketch = () => {
+  const collorCount = random.rangeFloor(1, 5
+    );
+  const pallete = random.pick(palletes).slice(0,collorCount);
   const createGrid = () => {
     const points = [];
-    const count = 20;
+    const count = 30;
     for(let x = 0; x < count; x++){
       for(let y = 0; y < count; y++){
         const u = x / (count -1);
         const v = y / (count -1);
-        points.push([u, v]);
+        points.push({
+          color: random.pick(pallete),
+          radius: Math.abs(0.01 +random.gaussian() * 0.01),
+          position: [u, v]
+        });
       }
     }
     return points;  
   };
 
-  const points =  createGrid();
+  random.setSeed(512)
+  const points =  createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach(data => {
+      const {radius, position, color} = data;
+      const [u, v] = position;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
-      context.strokeStyle = "black";
-      context.lineWidth = 40;
-      context.stroke();
+      context.arc(x, y, radius *  width, 0, Math.PI * 2, false);
+      context.fillStyle = color;
+      context.fill();
     })
   };
 };
